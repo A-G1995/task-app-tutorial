105237014c580409369dc45392079ab3dc2d6bce
Aryan <aryan.gholamlou@gmail.com>
2024-03-03 14:40:09 +0330

    initial commit

---
+++ src/main/kotlin/com/example/task_app_be_mediumcom/data/Task.kt
@@ -0,0 +1,38 @@
+package com.example.task_app_be_mediumcom.data
+
+import com.example.task_app_be_mediumcom.data.model.Prority
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import java.time.LocalDateTime
+
+
+@Entity
+@Table(
+    name = "task",
+    uniqueConstraints = [UniqueConstraint(name = "uk_task_description", columnNames = ["description"])]
+)
+class Task {
+    @Id
+    @GeneratedValue(generator = "task_sequence", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "task_sequence", sequenceName = "task_sequence", allocationSize = 1)
+    val id: Long = 0
+
+    @NotBlank
+    @Column(name = "description", nullable = false, unique = false)
+    var description: String = ""
+
+    @Column(name = "is_reminder_set", nullable = false)
+    var isReminderSet: Boolean = false
+
+    @Column(name = "is_task_open", nullable = false)
+    var isTaskOpen: Boolean = true
+
+    @Column(name = "created_on", nullable = false)
+    var createdOn: LocalDateTime = LocalDateTime.now()
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
+    var priority: Prority = Prority.LOW
+
+
+}
